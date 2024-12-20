f0a4d02eaa7ce58b40cf388f6334ae9e9abe0dbd
LukasCH09 <lukas.bitter@gmail.com>
2024-12-20 21:21:42 +0100

    Revert "Create download_json.yml"
    This reverts commit f53cd9bdf70509d5ef73fbf6d4b59ba6a19787a4.
    
    Revert "Bump vite in the npm_and_yarn group across 1 directory"
    
    This reverts commit acc03df36e4e4fef494ba3c2da87654a4395b527.
    
    Revert "typo"
    
    This reverts commit 37c27d13de3e4f67697816f0037b53e6f2efb1d1.
    
    Revert "Update download_json.yml"
    
    This reverts commit 3c3a9cf618b26d995f7147d71102534bd90cf539.
    
    Revert "typo"
    
    This reverts commit bf210c46fcf539aba7cee7e1a783caa0777d6c26.
    
    Revert "add node fetch"
    
    This reverts commit 00bcaeb5632850801d1548909d55a8a7e26d2b74.
    
    Revert "Update download_json.yml"
    
    This reverts commit baf2282206655e2df2e95799ec3bf07dff9cf868.
    
    Revert "Update download_json.yml"
    
    This reverts commit 3568d0a0e843bbaafde10b09d9d78249dfe8f215.
    
    Revert "Update download_json.yml"
    
    This reverts commit 10dbb4505c8f46d8f1c8a22df348d4b415c13c0d.
    
    Revert "Update download_json.yml"
    
    This reverts commit 1aadf03bea29a5ac1f2a32ef07cd2ce65a65229b.
    
    Revert "Update download_json.yml"
    
    This reverts commit d40e0fdbdf4fbed16dc39c3f545498d5748bc454.
    
    Revert "Update download_json.yml"
    
    This reverts commit 6687bae13b282c0ba839633c4a394a2ff87240e0.
    
    Revert "Update download_json.yml"
    
    This reverts commit c01fe59e38e83bffd47ba09229bef8b7b0b80677.
    
    Revert "update workflow"
    
    This reverts commit 734c2d82ea9a3227908b2479021a1d6da9f0230f.
    
    Revert "update workflow"
    
    This reverts commit 806770bbf6a54df3457ced5762849d57359dbdd8.
    
    Revert "update workflow"
    
    This reverts commit bc234cfcec1124958af399dbab869d86f17a964c.
    
    Revert "update workflow"
    
    This reverts commit 69444ff47ff148b1903b3bfe556869cf7a6ce324.
    
    Revert "update workflow"
    
    This reverts commit 0dba0998b96cea8e2068066eda1b478aef9e8498.

--- src/download_json.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch';
-import { writeFileSync } from 'fs';
-
-fetch('https://play.fiba3x3.com/api/v2/search/events?name=&input=&when=future&distance=1000&pageNum=1')
-    .then(response => response.json())
-    .then(data => {
-        writeFileSync('daily_tournament_fetch.json', JSON.stringify(data, null, 2));
-    })
-    .catch(error => console.error('Error fetching JSON:', error));
