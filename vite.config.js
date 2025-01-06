74ab0172096e7a791ec7c3beafdd21bdc8ee80c9
LukasCH09 <lukas.bitter@gmail.com>
2025-01-06 23:13:38 +0100

    deployment

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,14 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd(), '');
+  const {GOOGLE_MAPS_API_KEY = ''} = loadEnv(mode, process.cwd(), '');
+  const {IPINFO_TOKEN = ''} = loadEnv(mode, process.cwd(), '');
 
   return {
+    define: {
+      'process.env.GOOGLE_MAPS_API_KEY': JSON.stringify(GOOGLE_MAPS_API_KEY),
+      'process.env.IPINFO_TOKEN': JSON.stringify(IPINFO_TOKEN)
+    },
     base: '/fiba3x3map/',
     resolve: {
       alias: {
@@ -39,9 +45,6 @@ export default defineConfig(({mode}) => {
           rewrite: (path) => path.replace(/^\/ipinfo/, '')
         }
       }
-    },
-    define: {
-      'process.env': env
     }
   };
 });
